eaad405cf9522e2229bcb3efce3c3544b9d61a94
Resized <shai.yarden@gmail.com>
2022-06-19 14:26:32 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed);
+
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        
+    }
+}
